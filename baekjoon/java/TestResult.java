f719e862a3695c8700437295ce8e65dc54cb43cf
Ryujeuk <jeuk817@gmail.com>
2020-09-24 01:40:48 +0900

    feat: TestResult complete

---
+++ baekjoon/java/TestResult.java
@@ -0,0 +1,12 @@
+package baekjoon.java;
+
+import java.util.Scanner;
+
+public class TestResult {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+		String result = score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 60 ? "D" : "F";
+		System.out.println(result);
+	}
+}
